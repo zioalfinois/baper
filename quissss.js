a38273b5e089a8c1e65df9f7729a7db4adfcd7a3
zxx <zioalfinois4@gmail.com>
2025-01-16 10:48:54 +0700

    Add files via upload

---
+++ quissss.js
@@ -0,0 +1,19 @@
+soal//3
+const bazma = {
+    proker: 'PPLG',
+    konsentrasi: 'SIJA',
+    program: '4 tahun',
+    kurikulum: 'Merdeka',
+};
+
+console.log(`halo,jurusan saya ${bazma.konsentrasi} sedang ujian praktek ${bazma.proker} bersama Pak Dzikri`);
+console.log(`saya angkatan ke ${bazma.program} di bazma`);
+
+const saya = {
+    nama: 'zio.alfinois',
+    asal: 'cirebon',
+    umur: 16,
+    kelas: 10,
+};
+
+console.log(`halo,nama saya ${saya.nama},saya berasal dari ${saya.asal} ,umur saya ${saya.umur} tahun,saya kelas ${saya.kelas} di SMK TI BAZMA`);
